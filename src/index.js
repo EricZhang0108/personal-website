1ec054aca19d3ff35a388cee64bfb4ae9484cc5b
Eric Zhang <ericzhang0108@gmail.com>
2019-04-12 23:22:27 -0400

    release v1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+let t = 0;
+
+setInterval(runFunction, 1000);
+
+function runFunction() {
+    $('#main').html('You have been on this page for ' + t + ' seconds.');
+    t = t + 1;
+}
